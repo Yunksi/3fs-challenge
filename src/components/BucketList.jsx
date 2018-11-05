066e8192f762d66c4a85b701f156a01ae93b321c
Rok Erjavec <rokerjavec@yahoo.com>
2018-11-05 22:34:01 +0100

    Created components for listing buckets and viewing

---
+++ src/components/BucketList.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'reactstrap';
+
+export default class BucketList extends Component {
+  render() {
+    return (
+      <Row>
+        <Col>
+          <h1>Bucket list</h1>
+        </Col>
+      </Row>
+    );
+  }
+}
---
+++ src/components/BucketView.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BucketView = () => {
+  return <div>This is bucket view</div>;
+};
+
+export default BucketView;
