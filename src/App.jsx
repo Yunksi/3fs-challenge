434b1fd8b9222e0bec563fc4266830441b3b0ce4
Rok Erjavec <rokerjavec@yahoo.com>
2018-11-05 20:41:44 +0100

    Setup entry points

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return <div>I live inside app component.hihi,haha.</div>;
+  }
+}
+
+export default App;
