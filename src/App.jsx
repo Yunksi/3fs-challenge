9d89d78a46bb1b0a175d873b445c254aa7dffc51
Rok Erjavec <rokerjavec@yahoo.com>
2018-11-07 22:58:55 +0100

    Added styling to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { Container } from 'reactstrap';
 import NavBar from './components/NavBar';
 
 import MainContent from './components/MainContent';
+import './App.css';
 
 export class App extends Component {
   render() {
     return (
-      <div>
+      <div className="customContainer">
         <NavBar />
         <Container>
           <MainContent />
